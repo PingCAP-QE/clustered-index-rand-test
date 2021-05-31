4177d8728abcf7dadbc0ec7a1132668c15ce0643
tangenta <tangenta@126.com>
2021-05-31 14:55:39 +0800

    add hook predicate match

---
+++ sqlgen/hook_pred.go
@@ -0,0 +1,42 @@
+package sqlgen
+
+var _ FnEvaluateHook = (*FnHookPred)(nil)
+
+type FnHookPred struct {
+	FnHookDefault
+	toMatchFns []Fn
+	matched    bool
+}
+
+func (d *FnHookPred) BeforeEvaluate(state *State, fn Fn) Fn {
+	return fn
+}
+
+func (d *FnHookPred) AfterEvaluate(state *State, fn Fn, result string) string {
+	if state.invalid {
+		return result
+	}
+	for _, mf := range d.toMatchFns {
+		if mf.Equal(fn) {
+			d.matched = true
+			break
+		}
+	}
+	return result
+}
+
+func (d *FnHookPred) ResetMatched() {
+	d.matched = false
+}
+
+func (d *FnHookPred) Matched() bool {
+	return d.matched
+}
+
+func (d *FnHookPred) AddMatchFn(fn Fn) {
+	d.toMatchFns = append(d.toMatchFns, fn)
+}
+
+func NewFnHookPred() *FnHookPred {
+	return &FnHookPred{FnHookDefault: NewFnHookDefault("debug")}
+}
--- sqlgen/hook_test.go
+++ sqlgen/hook_test.go
@@ -1,9 +1,27 @@
 package sqlgen_test
 
 import (
+	"fmt"
+	"github.com/PingCAP-QE/clustered-index-rand-test/sqlgen"
 	. "github.com/pingcap/check"
 )
 
-func (s *testSuite) TestHookReplacer(c *C) {
+func (s *testSuite) TestHookPredNeedRollBackStmt(c *C) {
+	state := sqlgen.NewState()
+	predHook := sqlgen.NewFnHookPred()
+	rollBackStmts := []sqlgen.Fn{
+		sqlgen.DDLStmt, sqlgen.FlashBackTable, sqlgen.CreateTable, sqlgen.SplitRegion,
+	}
+	for _, f := range rollBackStmts {
+		predHook.AddMatchFn(f)
+	}
+	state.AppendHook(predHook)
 
+	for i := 0; i < 100; i++ {
+		query := sqlgen.Start.Eval(state)
+		if predHook.Matched() {
+			fmt.Println(query)
+		}
+		predHook.ResetMatched()
+	}
 }
