243b2b6a85f2e8b00dda85c2cea8de83c601cec7
ekexium <eke@fastmail.com>
2022-11-30 13:45:03 +0800

    support testnt cmd
    Signed-off-by: ekexium <eke@fastmail.com>

--- cmd.go
+++ cmd.go
@@ -158,6 +158,7 @@ func abtestCmd() *cobra.Command {
 		logPath     string
 		seed        string
 		debug       bool
+		testNT      bool
 	)
 	cmd := &cobra.Command{
 		Use:           "abtest",
@@ -166,23 +167,74 @@ func abtestCmd() *cobra.Command {
 		SilenceUsage:  true,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			parsedSeed := parseAndSetSeed(seed)
-
 			conn1 := setUpDatabaseConnection(dsn1)
 			conn2 := setUpDatabaseConnection(dsn2)
 
-			state := cases.NewMultiSchemaChangeState()
+			state := sqlgen.NewState()
+			if testNT {
+				state.SetWeight(sqlgen.DMLStmt, 500)
+				state.SetWeight(sqlgen.Query, 0) // there can be valid but randomized results
+				state.SetWeight(sqlgen.CommonUpdate, 0)
+				state.SetWeight(sqlgen.CommonDelete, 0)
+				state.SetWeight(sqlgen.NonTransactionalDelete, 1)
+				state.SetWeight(sqlgen.NonTransactionalUpdate, 1)
+				state.SetWeight(sqlgen.AlterColumn, 0) // column name change can have different results in nt-delete, e.g. shard on the changed column
+				state.SetWeight(sqlgen.ColumnDefinitionTypesJSON, 0)
+				state.ReplaceRule(sqlgen.SubSelect, sqlgen.SubSelectWithGivenTp)
+			} else {
+				state = cases.NewGBKState()
+			}
 			queries := generateInitialSQLs(state)
 			queries = append(queries, generatePlainSQLs(state, stmtCount)...)
+			if testNT {
+				for i := 0; i < 10; i++ {
+					query, err := sqlgen.QueryAll.Eval(state)
+					if err != nil {
+						return err
+					}
+					queries = append(queries, query)
+				}
+				executeQuery(conn1, "set tidb_general_log=1")
+				executeQuery(conn2, "set tidb_general_log=1")
+			}
 
-			for _, query := range queries {
+			for i, query := range queries {
+				testNT := testNT && strings.HasPrefix(query, "batch on")
 				if debug {
-					fmt.Println(query + ";")
+					fmt.Println(query + ";\n")
 				}
 				rs1, err1 := executeQuery(conn1, query)
+				if testNT {
+					if strings.Contains(query, " delete ") {
+						query = query[strings.Index(query, "delete"):]
+					} else if strings.Contains(query, " update ") {
+						query = query[strings.Index(query, "update"):]
+					} else {
+						panic("unexpected query: delete or update not found")
+					}
+				}
 				rs2, err2 := executeQuery(conn2, query)
-				if debug {
-					fmt.Println(colorizeErrorMsg(err1))
-					fmt.Println(colorizeErrorMsg(err2))
+				if testNT {
+					// If either of the normal dml or nt-dml fails, we should not compare the results. We cannot guarantee that the results are the same.
+					// because we assure there is at most 1 batch. Errors in nt-dml are always early returned, thus captured in err1.
+					if err2 != nil || err1 != nil {
+						// skip this case, see BU-32.
+						fmt.Println("one of the statements failed, skip this case:")
+						if err1 != nil {
+							fmt.Printf("err1: %v, ", err1)
+						}
+						if err2 != nil {
+							fmt.Printf("err2: %v", err2)
+						}
+						return nil
+					}
+					if rs1 != nil && rs2 != nil && debug {
+						var a, b bytes.Buffer
+						rs1.PrettyPrint(&a)
+						rs2.PrettyPrint(&b)
+						println(a.String(), b.String())
+					}
+					continue
 				}
 				if !ValidateErrs(err1, err2) {
 					msg := fmt.Sprintf("error mismatch: %v != %v\nseed: %d\nquery: %s", err1, err2, parsedSeed, query)
@@ -195,7 +247,15 @@ func abtestCmd() *cobra.Command {
 					fmt.Println(rs1.String())
 					fmt.Println(rs2.String())
 				}
+				if testNT {
+					continue
+				}
 				if err := compareResult(rs1, rs2, query); err != nil {
+					logFile, _ := os.Create("case.sql")
+					for j := 0; j <= i; j++ {
+						logFile.WriteString(fmt.Sprintf("%s;\n", queries[j]))
+					}
+					logFile.Close()
 					return err
 				}
 			}
@@ -209,6 +269,7 @@ func abtestCmd() *cobra.Command {
 	cmd.Flags().StringVar(&logPath, "log", "", "The output of 2 databases")
 	cmd.Flags().StringVar(&seed, "seed", "1", "random seed")
 	cmd.Flags().BoolVar(&debug, "debug", false, "print generated SQLs")
+	cmd.Flags().BoolVar(&testNT, "nontransactional", false, "test non-transactional dml")
 	return cmd
 }
 
